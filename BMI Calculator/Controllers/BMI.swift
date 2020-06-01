8c660a9e2377634b07a0a81437953df7f2dc0d3b
Elvis Adorkor <elvisedem98@gmail.com>
2020-06-01 11:51:11 -0400

    simple bmi calculator iOS app

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Elvis Adorkor on 5/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
